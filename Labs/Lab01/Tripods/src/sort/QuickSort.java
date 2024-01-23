0e827a1704ea8ccd8fbf8cc379b991ebbfa82d6e
EdenSolll <erg9359g.rit.edu>
2024-01-23 12:32:57 -0500

    saving progress incase everything goes wrong

---
+++ Labs/Lab01/Tripods/src/sort/QuickSort.java
@@ -0,0 +1,99 @@
+package sort;
+
+import tripods.Tripod;
+
+import java.util.ArrayList;
+
+/**
+ * Perform an "out of place" quick sort on an array list of Tripod's by
+ * ascending tripod sum.
+ * <pre>
+ * quick_sort (not in place):
+ * best=O(nlogn)
+ * worst=O(n^2)
+ * </pre>
+ *
+ * @author RIT CS
+ * @author Eden Grace
+ */
+public class QuickSort {
+    /**
+     * Partition the array for values less than the pivot.
+     *
+     * @param data  the full array of data
+     * @param pivot the pivot
+     * @return data less than the pivot
+     */
+    public static ArrayList<Tripod> partitionLess(ArrayList<Tripod> data, Tripod pivot) {
+        ArrayList<Tripod> less_tripods = new ArrayList<>();
+        for (Tripod datum : data) {
+            if (datum.sum() < pivot.sum()) {
+                less_tripods.add(datum);
+            }
+        }
+        return less_tripods;
+    }
+
+    /**
+     * Partition the array for values equal to the pivot.
+     *
+     * @param data  the full array of data
+     * @param pivot the pivot
+     * @return data equal to the pivot
+     */
+    public static ArrayList<Tripod> partitionEqual(ArrayList<Tripod> data, Tripod pivot) {
+        ArrayList<Tripod> equal_tripods = new ArrayList<>();
+        for (Tripod datum : data) {
+            if (datum.sum() == pivot.sum()) {
+                equal_tripods.add(datum);
+            }
+        }
+        return equal_tripods;
+    }
+    /**
+     * Partition the array for values greater than the pivot.
+     *
+     * @param data  the full array of data
+     * @param pivot the pivot
+     * @return data greater than the pivot
+     */
+    public static ArrayList<Tripod> partitionGreater(ArrayList<Tripod> data, Tripod pivot) {
+        ArrayList<Tripod> greater_tripods = new ArrayList<>();
+        for (Tripod datum : data) {
+            if (datum.sum() > pivot.sum()) {
+                greater_tripods.add(datum);
+            }
+        }
+        return greater_tripods;
+    }
+
+    /**
+     * Performs a quick sort and returns a newly sorted array.
+     *
+     * @param data the data to be sorted
+     * @return a sorted array
+     */
+    public static ArrayList<Tripod> quickSort(ArrayList<Tripod> data) {
+
+       ArrayList<Tripod> sorted_array = new ArrayList<>();
+
+        if (data.isEmpty()) {
+            return data;
+        } else {
+            Tripod pivot = data.getFirst();
+            ArrayList<Tripod> less = partitionLess(data, pivot);
+            ArrayList<Tripod> equal = partitionEqual(data, pivot);
+            ArrayList<Tripod> greater = partitionGreater(data, pivot);
+            if (less.size() > 1) {
+                less = quickSort(less);
+            }
+            if (greater.size() > 1) {
+                 greater = quickSort(greater);
+            }
+            sorted_array.addAll(less);
+            sorted_array.addAll(equal);
+            sorted_array.addAll(greater);
+        }
+        return sorted_array;
+    }
+}
---
+++ Labs/Lab01/Tripods/src/tripods/Tripods.java
@@ -0,0 +1,242 @@
+package tripods;
+import sort.QuickSort;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * A program that finds the optimal placement of a number of tripods in an
+ * N * M grid of numbers.  A tripod can touch three adjacent cells, based on orientation,
+ * e.g. a north facing tripod touches the east, south and west cells.
+ * The goal is to find the placement of a number of tripods, such that the
+ * total sums of the cells that all combined tripods touch is maximum.
+ *
+ * @author RIT CS
+ * @author Eden Grace
+ */
+public class Tripods {
+    /** there are 4 corners where a tripod cannot be placed in any direction */
+    public final static int CORNERS = 4;
+    /** when the number of cells exceeds this number it won't be displayed to standard output */
+    public final static int MAX_CELLS_TO_DISPLAY = 10000;
+
+    /**
+     * Read the 2-D number grid into a 2 dimension native array. This method should
+     * print the following to standard output:
+     * Rows: #, Columns: #
+     *
+     * @param filename the file with the number grid
+     * @return the newly created 2-D native array of numbers
+     * @throws IOException if the file cannot be found or there is an error reading
+     */
+    public static int[][] loadGrid(String filename) throws IOException {
+        Scanner file = new Scanner(new File(filename));
+        int y = file.nextInt();
+        int x = file.nextInt();
+        int[][] grid = new int[y][x];
+        System.out.println("Rows: " + y + ", Columns: " + x);
+        for (int a = 0; a < y; a++) {
+            for (int b = 0; b < x; b++) {
+                grid[a][b] = file.nextInt();
+            }
+        }
+        file.close();
+        return grid;
+    }
+
+    /**
+     * Get the number of rows in the grid.
+     *
+     * @param grid the 2-D grid of numbers
+     * @return number of rows
+     */
+    public static int getNumRows(int[][] grid) {
+        return grid.length;
+    }
+
+    /**
+     * Get the number of columns in the grid.
+     *
+     * @param grid the 2-D grid of numbers
+     * @return number of columns
+     */
+    public static int getNumColumns(int[][] grid) { return grid[0].length;
+    }
+
+    /**
+     * Get the maximum number of tripods that could be placed on the board.
+     * A tripod cannot be placed in any of the four corners of the grid in
+     * any orientation.
+     *
+     * @param grid the 2-D grid of numbers
+     * @return the maximum number of tripods that can placed in the grid
+     */
+    public static int getMaxTripods(int[][] grid) {
+        return grid[0].length * grid.length - CORNERS;
+    }
+
+    /**
+     * Display the grid to standard output, only if the number of cells is less
+     * than or equal to MAX_CELLS_TO_DISPLAY.  If that size exceeds, print
+     * "Too many cells to display" instead.  For example with tripods-3.txt:
+     *  0 3 7 9
+     *  2 5 1 4
+     *  3 3 2 1
+     *  4 6 8 4
+     *  4 1 2 0
+     *
+     * @param grid the 2-D grid of numbers
+     */
+    public static void displayGrid(int[][] grid) {
+        if (grid.length * grid[0].length <= MAX_CELLS_TO_DISPLAY) {
+            for (int i = 0; i < grid.length; i++) {
+                for (int n = 0; n < grid[0].length - 1; n++) {
+                    System.out.print(grid[i][n] + " ");
+                }
+                System.out.println(grid[i][grid[0].length -1]);
+            }
+        } else {
+            System.out.println("Grid is too large to display");
+        }
+    }
+
+    /**
+     * A spot is valid if the tripod can be placed in the cell at (row, col),
+     * taking into account the direction of the tripod and the location of its
+     * three legs.
+     *
+     * @param grid the 2-D grid of numbers
+     * @param row tripod's row (centered)
+     * @param col tripod's column (centered)
+     * @param dir the direction the tripod is facing
+     * @return whether the tripod can be placed in the spot or not
+     */
+    public static boolean isSpotValid(int[][] grid, int row, int col, Direction dir) {
+        boolean isTopEdge = row == 0;
+        boolean isBottomEdge = row == grid.length - 1;
+        boolean isLeftEdge = col == 0;
+        boolean isRightEdge = col == grid[0].length - 1;
+
+        if ((isTopEdge || isBottomEdge) && (isLeftEdge || isRightEdge)) {
+            return false;
+        }
+        if (dir == Direction.SOUTH && isTopEdge) {
+            return true;
+        }
+        if (dir == Direction.NORTH && isBottomEdge) {
+            return true;
+        }
+        if (dir == Direction.EAST && isLeftEdge) {
+            return true;
+        }
+        if (dir == Direction.WEST && isRightEdge) {
+            return true;
+        }
+        return !(isTopEdge || isBottomEdge || isLeftEdge || isRightEdge);
+    }
+
+    /**
+     * Given a tripod at a location in the grid and facing a certain direction,
+     * sum the numbers in the grid that the three legs of the tripod stand on.
+     *
+     * @param grid the 2-D grid of numbers
+     * @param row tripod's row (centered)
+     * @param col tripod's column (centered)
+     * @param dir the direction the tripod is facing
+     *
+     * @rit.pre spot must be valid
+     *
+     * @return the sum of the tripods legs
+     */
+    public static int getSum(int[][] grid, int row, int col, Direction dir) {
+        boolean isTopEdge = row == 0;
+        boolean isBottomEdge = row == grid.length - 1;
+        boolean isLeftEdge = col == 0;
+        boolean isRightEdge = col == grid[0].length - 1;
+        boolean isEdge = isTopEdge || isBottomEdge || isLeftEdge || isRightEdge;
+
+        switch (dir) {
+            case NORTH:
+                if (!isEdge || isBottomEdge) {
+                    return grid[row][col - 1] + grid[row][col + 1] + grid[row - 1][col];
+                }
+            case SOUTH:
+                if (!isEdge || isTopEdge) {
+                    return grid[row][col - 1] + grid[row][col + 1] + grid[row + 1][col];
+                }
+            case EAST:
+                if (!isEdge || isLeftEdge) {
+                    return grid[row + 1][col] + grid[row - 1][col] + grid[row][col + 1];
+                }
+            case WEST:
+                if (!isEdge || isRightEdge) {
+                    return grid[row + 1][col] + grid[row - 1][col] + grid[row][col - 1];
+                }
+            default:
+                return 0;
+        }
+    }
+    public static ArrayList<Tripod> generateSums(int[][] grid) {
+        int x = getNumColumns(grid);
+        int y = getNumRows(grid);
+        ArrayList<Tripod> valid_tripods = new ArrayList<>();
+        for (int a = 0; a < y; a++) {
+            for (int b = 0; b < x; b++) {
+                ArrayList<Tripod> max_at_cord = new ArrayList<>();
+                for (Direction dir : Direction.values()) {
+                    if (isSpotValid(grid, a, b, dir)) {
+                        int tripod_sum = getSum(grid, a, b, dir);
+                        max_at_cord.add(new Tripod(a, b, dir, tripod_sum));
+                    }
+                }
+                if (!max_at_cord.isEmpty()) {
+                    valid_tripods.add(QuickSort.quickSort(max_at_cord).getLast());
+                }
+            }
+        }
+        return valid_tripods;
+    }
+
+    public static void displayOptimalPlacements(ArrayList<Tripod> tripods, int numTripods) {
+        int total_sum = 0;
+        for (int i = 0, j = tripods.size() - 1; i < numTripods; i++, j--) {
+            System.out.println(i+1 + ": location: (" + tripods.get(j).row() + "," + tripods.get(j).col() + "), direction: " + tripods.get(j).dir() + ", sum: " + tripods.get(j).sum());
+            total_sum += tripods.get(j).sum();
+        }
+        System.out.println("Total sum: " +total_sum);
+    }
+
+    /**
+     * The main program takes the file name from the command line.  It then
+     * loads the file into a 2-D native array which is then displayed.
+     * Next the user is prompted for how many tripods they want for
+     * the optimal sum.  If the number of tripods does not exceed the
+     * total number that can be placed, the optimal tripods by location
+     * are generated, then sorted by descending sum and then displayed
+     * to the user.
+     *
+     * @param args command line arguments (unused)
+     */
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Tripods filename");
+        } else {
+            try {
+                int[][] grid = loadGrid(args[0]);
+                displayGrid(grid);
+                Scanner count_input = new Scanner(System.in);
+                System.out.print("Number of tripods: ");
+                int tripod_count = count_input.nextInt();
+                if (tripod_count <= getMaxTripods(grid)) {
+                    displayOptimalPlacements(QuickSort.quickSort(generateSums(grid)), tripod_count);
+                } else {
+                    System.out.println("Too many tripods to place!");
+                }
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+}
